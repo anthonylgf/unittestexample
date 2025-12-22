23bade8c146768b9e5f026f43f27b290e3f7d799
KarineFBrito <karineferreiradebrito@gmail.com>
2025-12-22 12:38:00 -0300

    Alterações nas configurações do kafka no código.

--- src/test/java/com/example/unittestexample/cucumber/TestRunner.java
+++ src/test/java/com/example/unittestexample/cucumber/TestRunner.java
@@ -6,7 +6,6 @@ import org.junit.platform.suite.api.*;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @Suite
--- src/test/java/com/example/unittestexample/cucumber/configuration/CucumberConfiguration.java
+++ src/test/java/com/example/unittestexample/cucumber/configuration/CucumberConfiguration.java
@@ -1,18 +1,21 @@
 package com.example.unittestexample.cucumber.configuration;
 
+import com.example.unittestexample.UnittestexampleApplication;
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+@ActiveProfiles("docker-local")
+@SpringBootTest(
+    classes = UnittestexampleApplication.class,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @CucumberContextConfiguration
 @ContextConfiguration(
     initializers = ConfigDataApplicationContextInitializer.class,
