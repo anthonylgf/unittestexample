f160d65a5891f5e4391490d948369b359409c4cc
KarineFBrito <karineferreiradebrito@gmail.com>
2026-03-12 13:02:09 -0300

    Alterando as configurações do Redis localmente e aws.

--- src/main/java/com/example/unittestexample/configs/RedisConfig.java
+++ src/main/java/com/example/unittestexample/configs/RedisConfig.java
@@ -1,6 +1,8 @@
 package com.example.unittestexample.configs;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,10 +16,19 @@ public class RedisConfig {
   @Bean
   public RedisCacheConfiguration cacheConfiguration(ObjectMapper objectMapper) {
 
+    ObjectMapper redisMapper = new ObjectMapper();
+    redisMapper.registerModule(new JavaTimeModule());
+
+    // Esta é a "etiqueta" que permite ao Redis reconhecer o tipo Aluno
+    redisMapper.activateDefaultTyping(
+        redisMapper.getPolymorphicTypeValidator(),
+        ObjectMapper.DefaultTyping.NON_FINAL,
+        JsonTypeInfo.As.PROPERTY);
+
     return RedisCacheConfiguration.defaultCacheConfig()
         .entryTtl(Duration.ofMinutes(10))
         .serializeValuesWith(
             RedisSerializationContext.SerializationPair.fromSerializer(
-                new GenericJackson2JsonRedisSerializer(objectMapper)));
+                new GenericJackson2JsonRedisSerializer(redisMapper)));
   }
 }
