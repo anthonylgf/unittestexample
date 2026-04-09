4d8a2b11f9ca162c6e130a47a44c9b0b6235fd07
KarineFBrito <karineferreiradebrito@gmail.com>
2026-04-09 14:17:03 -0300

    Atualizando os testes unitários.

---
+++ src/test/java/com/example/unittestexample/controllers/TurmaControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.unittestexample.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(TurmaController.class)
+public class TurmaControllerTest {
+}
