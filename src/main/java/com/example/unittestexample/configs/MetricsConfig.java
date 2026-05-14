e680bfa132d8b3be6277c7e7ec73e22170fa9cce
KarineFBrito <karineferreiradebrito@gmail.com>
2026-05-14 11:34:20 -0300

    Adicionando metricas no AlunoService (#19)

---
+++ src/main/java/com/example/unittestexample/configs/MetricsConfig.java
@@ -0,0 +1,20 @@
+package com.example.unittestexample.configs;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+  @Bean
+  MeterRegistryCustomizer<MeterRegistry> commonTags(MetricsProperties properties) {
+    return registry ->
+        registry
+            .config()
+            .commonTags(
+                "application", properties.getName(),
+                "environment", properties.getEnvironment());
+  }
+}
---
+++ src/main/java/com/example/unittestexample/configs/MetricsProperties.java
@@ -0,0 +1,16 @@
+package com.example.unittestexample.configs;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+@Getter
+@Setter
+public class MetricsProperties {
+
+  private String name;
+  private String environment;
+}
